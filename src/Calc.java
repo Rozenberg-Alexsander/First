96c5015d5e718febcc2e9b7422fe0db23b844a57
Alex <rozenberg.s@mail.ru>
2023-01-18 22:53:18 +0300

    Second commit

--- src/Calc.java
+++ src/Calc.java
@@ -10,10 +10,13 @@ public class Calc {
         System.out.println("Введите второе чило: ");
         float num2 = scan.nextFloat();
 
-        float res1 = num1 + num2;
-        float res2 = num1 - num2;
-        float res3 = num1 * num2;
-        float res4 = num1 / num2;
+        System.out.println("Введите третье чило: ");
+        float num3 = scan.nextFloat();
+
+        float res1 = num1 + num2 + num3;
+        float res2 = num1 - num2 - num3;
+        float res3 = num1 * num2 * num3;
+        float res4 = num1 / num2 / num3;
 
         System.out.println("Результат: ");
         System.out.println(res1 + "\n" + res2 + "\n" + res3 + "\n" + res4 );
